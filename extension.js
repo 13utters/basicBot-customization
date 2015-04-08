c40909b4e4e60278504249e58741e0c455d13280
ureadmyname <ureadmyemailname@gmail.com>
2015-04-08 11:53:47 +0930

    Update extension.js

--- extension.js
+++ extension.js
@@ -54,26 +54,25 @@
                 ],
                     functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    if (!bot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var msg = chat.message;
-
                         var space = msg.indexOf(' ');
                         if (space === -1) {
-                            API.sendChat(basicBot.chat.eatcandy);
+                            API.sendChat(bot.chat.eatcandy);
                             return false;
                         }
                         else {
                             var name = msg.substring(space + 2);
-                            var user = basicBot.userUtilities.lookupUserName(name);
+                            var user = bot.userUtilities.lookupUserName(name);
                             if (user === false || !user.inRoom) {
-                                return API.sendChat(subChat(basicBot.chat.nousercandy, {name: name}));
+                                return API.sendChat(subChat(bot.chat.nousercandy, {name: name}));
                             }
                             else if (user.username === chat.un) {
-                                return API.sendChat(subChat(basicBot.chat.selfcandy, {name: name}));
+                                return API.sendChat(subChat(bot.chat.selfcandy, {name: name}));
                             }
                             else {
-                                return API.sendChat(subChat(basicBot.chat.candy, {nameto: user.username, namefrom: chat.un, candy: this.getCandy()}));
+                                return API.sendChat(subChat(bot.chat.candy, {nameto: user.username, namefrom: chat.un, candy: this.getCandy()}));
                             }
                         }
                     }
