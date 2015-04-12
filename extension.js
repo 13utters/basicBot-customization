878a99d74f23a74947edd2f8a8cfbca5331fc36f
ureadmyname <ureadmyemailname@gmail.com>
2015-04-12 23:39:36 +0930

    Update extension.js

--- extension.js
+++ extension.js
@@ -33,6 +33,18 @@
          }
 
          */
+             bot.commands.whatissubCommand = {
+	command: 'killtroll',
+	rank: 'manager',
+	type: 'exact',
+	functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+API.sendChat("/me http://i.imgur.com/bwNSflr.jpg");
+}
+}
+};  
         
               bot.commands.whatissubCommand = {
 	command: 'whatissub',
