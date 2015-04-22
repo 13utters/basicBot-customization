b22304221b1f23cfffff5c47b9f485091abcc282
Jason <ureadmyemailname@gmail.com>
2015-04-23 09:25:59 +0930

    Update extension.js

--- extension.js
+++ extension.js
@@ -86,7 +86,7 @@ functionality: function (chat, cmd) {
 
              bot.commands.killtrollCommand = {
 	command: 'killtroll',
-	rank: 'manager',
+	rank: 'bouncer',
 	type: 'exact',
 	functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
