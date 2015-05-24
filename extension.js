6e5b46aa303762c168e4ba15e53580bb72971486
Jason <ureadmyemailname@gmail.com>
2015-05-25 07:32:08 +0930

    Update extension.js

--- extension.js
+++ extension.js
@@ -113,6 +113,15 @@ if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0)
 if (!bot.commands.executable(this.rank, chat)) return void (0);
 else {API.sendChat("/me Do !dc if you have recently disconnected to get your position on the waitlist back!");}}};
 
+bot.commands.dropthedeskCommand = {
+command: 'dropthedesk',
+rank: 'user',
+type: 'exact',
+functionality: function (chat, cmd) {
+if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+if (!bot.commands.executable(this.rank, chat)) return void (0);
+else {API.sendChat("/me dropthedesk link");}}};
+
 bot.commands.djcycleinfoCommand = {
 command: 'djcycleinfo',
 rank: 'user',
