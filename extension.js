530b0e3a85366b3590774851ee6febb5163e569c
Jason <ureadmyemailname@gmail.com>
2015-06-12 13:51:07 +0930

    Update extension.js

--- extension.js
+++ extension.js
@@ -299,7 +299,7 @@ songstats: false,
 commandLiteral: "!",
 blacklists: {
 NSFW: "https://rawgit.com/ureadmyname/basicBot-customization/master/ExampleNSFWlist.json",
-OP: "https://rawgit.com/ureadmyname/basicBot-customization/master/ExampleOPlist.json"
+OP: "https://rawgit.com/ureadmyname/basicBot-customization/master/ExampleOPlist.json",
 BANNED: "https://rawgit.com/ureadmyname/basicBot-customization/master/ExampleBANNEDlist.json"}}));
 $.getScript("https://rawgit.com/ureadmyname/basicBot/master/basicBot.js", extend);
 }).call(this);
