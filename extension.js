54b0e436a6f84b4d3c1e16f9b26081b1d10424e9
Jason <ureadmyemailname@gmail.com>
2015-08-06 05:57:55 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -167,6 +167,15 @@ if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0)
 if (!bot.commands.executable(this.rank, chat)) return void (0);
 else {API.sendChat("/me Remember to click the star in the top bar to favorite this room!! http://i.imgur.com/sXDxQgj.png");}}};
 
+bot.commands.selloutCommand = {
+command: 'sellout',
+rank: 'user',
+type: 'exact',
+functionality: function (chat, cmd) {
+if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+if (!bot.commands.executable(this.rank, chat)) return void (0);
+else {API.sendChat("/me QTbot is going sellout mode! Interested in being a manager for life? Check out this link! http://git.io/vOSC2");}}};
+
 bot.commands.illuminatiCommand = {
 command: 'illuminati',
 rank: 'user',
