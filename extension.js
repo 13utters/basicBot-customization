0ae3f40773b2ec50800691889d78e177d52efa17
Jason <ureadmyemailname@gmail.com>
2015-08-30 03:40:34 +0930

    Update extension.js

--- extension.js
+++ extension.js
@@ -179,7 +179,7 @@ type: 'exact',
 functionality: function (chat, cmd) {
 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 if (!bot.commands.executable(this.rank, chat)) return void (0);
-else {API.sendChat("/me http://git.io/vGZFx");}}};
+else {API.sendChat("/me Staff Guidelines: http://git.io/vGZFx");}}};
 
 bot.commands.illuminatiCommand = {
 command: 'illuminati',
