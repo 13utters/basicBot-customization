15785393e556f47c2e826a838116f6c2b768156d
Jason <ureadmyemailname@gmail.com>
2016-08-07 07:18:37 +0930

    Update extension.js

--- extension.js
+++ extension.js
@@ -370,7 +370,7 @@ type: 'exact',
 functionality: function (chat, cmd) {
 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 if (!bot.commands.executable(this.rank, chat)) return void (0);
-else {API.sendChat("/me update log to recent changes of qtbot: https://git.io/v638C");}}};
+else {API.sendChat("/me update log to recent changes of qtbot: https://git.io/v63BR");}}};
 //
 bot.loadChat();}
 localStorage.setItem("basicBotsettings", JSON.stringify({
