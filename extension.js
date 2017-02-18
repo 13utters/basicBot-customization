77a97f450978b69aea80a1f5b72bea9bffe13453
Jason <ureadmyemailname@gmail.com>
2017-02-18 21:10:38 +1030

    Update extension.js

--- extension.js
+++ extension.js
@@ -141,7 +141,7 @@ type: 'exact',
 functionality: function (chat, cmd) {
 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 if (!bot.commands.executable(this.rank, chat)) return void (0);
-else {API.sendChat("/me Remember to click the star in the top bar to favorite this room!! http://i.imgur.com/chNflCb.png");}}};
+else {API.sendChat("/me Remember to click the star in the top bar to favorite this room!! http://i.imgur.com/MP0TxkT.png");}}};
 
 bot.commands.guidelinesCommand = {
 command: 'guidelines',
